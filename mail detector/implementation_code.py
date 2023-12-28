9b6edf685598362c363265231ff240f306853abb
Saddam705 <110493839+Saddam705@users.noreply.github.com>
2023-12-28 12:19:20 +0000

    this is the final pages and working fine

--- mail detector/implementation_code.py
+++ mail detector/implementation_code.py
@@ -1,9 +1,21 @@
+import pickle
 from flask import *
 app = Flask(__name__)
+
+cv = pickle.load(open('mail detector/cv.pkl','rb'))
+clf = pickle.load(open('mail detector/clf.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    email = request.form.get('content')
+    tokenized_email = cv.transform([email])
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction==1 else -1
+    return render_template('index.html',prediction=prediction,email = email)
 
 if __name__=='__main__':
     app.run(debug=True)
